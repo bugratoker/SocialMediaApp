202f195d8c71eea741cf7e43ba0bdb76ecf2e720
alp buğra toker <bugratoker11@gmail.com>
2023-05-11 02:22:56 +0300

    first

---
+++ CleanArchitecture/CleanArchitecture.Application/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Core.Entities
+{
+    public class User : AuditableBaseEntity
+    {
+        public UserRoles UserType { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public Boolean EmailConfirmed { get; set; }
+        public List<User> Followings { get; set; }
+        public List<User> Followers { get; set; }
+
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Application/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CleanArchitecture.Core.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+
+namespace CleanArchitecture.Core
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        }
+    }
+}
